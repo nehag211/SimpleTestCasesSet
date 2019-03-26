9a4b0b91d1c2a69d5d86efba76fb06ed6fe93ceb
Neha <Neha@9.81.222.18>
2019-03-26 13:14:15 +0530

    Medium Level Test Cases

--- Selenium-Automation-Elearning-Framework-TestNG/src/com/training/pom/LoginPOM.java
+++ Selenium-Automation-Elearning-Framework-TestNG/src/com/training/pom/LoginPOM.java
@@ -19,7 +19,7 @@ public class LoginPOM {
 	@FindBy(id = "password")
 	private WebElement password;
 
-	@FindBy(id = "formLogin_submitAuth")
+	@FindBy(id = "form-login_submitAuth")
 	private WebElement loginBtn;
 	
 	@FindBy(xpath = "//div[@class=\"normal-message\"]/p")
